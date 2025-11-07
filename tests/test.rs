50405ab963f944cb7ee9570ac851e2095645d82a
stellanomia <fulmlumo@gmail.com>
2025-11-07 20:38:23 +0900

    Fix test

--- tests/test.rs
+++ tests/test.rs
@@ -1,21 +1,22 @@
-use assert_cmd::prelude::*;
-use std::process::Command;
+use uroman::{RomFormat, Uroman};
+use std::{fs::File, io::BufReader};
 
 fn assert_uroman_output(input_path: &str, expected_output: &str) {
     let expected_output_normalized = expected_output.replace("\r\n", "\n");
 
-    let cmd = Command::new("uroman-rs")
-        .arg("-i")
-        .arg(input_path)
-        .unwrap();
-    let output = cmd
-        .assert()
-        .success()
-        .get_output()
-        .stdout
-        .clone();
+    let uroman = Uroman::new();
+    let mut buf = vec![];
+    uroman.romanize_file(
+        BufReader::new(File::open(input_path).unwrap()),
+        &mut buf,
+        None,
+        RomFormat::Str,
+        None,
+        false,
+        false,
+    ).unwrap();
 
-    let actual_output_normalized = String::from_utf8(output).unwrap().replace("\r\n", "\n");
+    let actual_output_normalized = String::from_utf8(buf).unwrap().replace("\r\n", "\n");
 
     assert_eq!(actual_output_normalized, expected_output_normalized);
 }
